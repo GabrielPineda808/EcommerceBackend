3a3bf4a91ae67589501a4c604608761d425eb7be
Gabe <gabepineda6@gmail.com>
2024-12-19 16:35:39 -0800

    Created the methods that will be needed for our profile dao

--- src/main/java/org/yearup/data/ProfileDao.java
+++ src/main/java/org/yearup/data/ProfileDao.java
@@ -1,9 +1,12 @@
 package org.yearup.data;
 
 
+import org.yearup.models.Category;
 import org.yearup.models.Profile;
 
 public interface ProfileDao
 {
     Profile create(Profile profile);
+    Profile getByUserId(int id);
+    void update(int profileId, Profile profile);
 }
