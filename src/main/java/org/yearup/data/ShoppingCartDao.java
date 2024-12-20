ea9f3558d810184f042dd8e2bbef7c6ac7026485
Gabe <gabepineda6@gmail.com>
2024-12-20 08:27:16 -0800

    Push to git

--- src/main/java/org/yearup/data/ShoppingCartDao.java
+++ src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -1,9 +1,13 @@
 package org.yearup.data;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.yearup.models.ShoppingCart;
 
 public interface ShoppingCartDao
 {
-    ShoppingCart getByUserId(int userId);
+    ShoppingCart getByUserId(int id);
     // add additional method signatures here
+    void addProductToCart(int userId, int productId);
+    void deleteProductFromCart(int userId);
+    void updateCart(int userId, int quantity, int productId);
 }
--- src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
@@ -1,4 +1,97 @@
 package org.yearup.data.mysql;
 
-public class MySqlShoppingCartDao {
+import org.springframework.stereotype.Component;
+import org.yearup.data.ProductDao;
+import org.yearup.data.ShoppingCartDao;
+import org.yearup.models.Product;
+import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+@Component
+public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDao {
+    private ProductDao productDao;
+    public MySqlShoppingCartDao(DataSource dataSource, ProductDao productDao) {
+        super(dataSource);
+        this.productDao = productDao;
+    }
+
+    @Override
+    public ShoppingCart getByUserId(int id) {
+        ShoppingCart shoppingCart = new ShoppingCart();
+        String q = "SELECT * FROM shopping_cart WHERE user_id = ?";
+        try (Connection connection = getConnection();
+             PreparedStatement ps = connection.prepareStatement(q);
+        ) {
+            ps.setInt(1, id);
+            try(ResultSet rs = ps.executeQuery();) {
+                while (rs.next()) {
+                    Product product = productDao.getById(rs.getInt("product_id"));
+                    ShoppingCartItem shoppingCartItem = new ShoppingCartItem();
+                    shoppingCartItem.setProduct(product);
+                    shoppingCartItem.setQuantity(rs.getInt("quantity"));
+                    shoppingCart.add(shoppingCartItem);
+                }
+            }
+            return shoppingCart;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void addProductToCart(int userId, int productId) {
+        String sql = " INSERT INTO shopping_cart (user_id, product_id, quantity) VALUES (?, ?, ?) ";
+
+        try (Connection connection = getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)
+        ) {
+            ps.setInt(1, userId);
+            ps.setInt(2, productId);
+            ps.setInt(3, 1);
+
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    public void deleteProductFromCart(int userId) {
+        String q = "DELETE FROM shopping_cart WHERE user_id = ?";
+        try(Connection connection = getConnection();
+            PreparedStatement ps = connection.prepareStatement(q);
+        ){
+            ps.setInt(1, userId);
+            int row = ps.executeUpdate();
+            System.out.println(row);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void updateCart(int userId, int quantity, int productId) {
+        String q = "UPDATE shopping_cart SET quantity = ? WHERE user_id = ? AND product_id = ?";
+        try(Connection connection = getConnection();
+            PreparedStatement ps = connection.prepareStatement(q)
+        ){
+            ps.setInt(1, quantity);
+            ps.setInt(2, userId);
+            ps.setInt(3, productId);
+
+            int row = ps.executeUpdate();
+            System.out.println(row);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
